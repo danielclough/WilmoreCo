f29575132984c69f6b76c17633872412cfcefd57
danielclough <danielclough@gmail.com>
2020-03-03 15:52:01 -0800

    fix title

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,9 +7,9 @@
 module.exports = {
   // Add common site data (not required by the theme, but recommended)
   siteMetadata: {
-    title: "Starter Site for Gatsby Theme Musician",
+    title: "Wilmore & Company",
     description: "",
-    siteUrl: "https://github.com/ekafyi/starter-musician-theme"
+    siteUrl: "https://thirsty-heisenberg-ebe805.netlify.com/"
   },
   plugins: [
     // Add gatsby-theme-musician to your site
