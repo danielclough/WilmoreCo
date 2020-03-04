712d133af35c5bc8cf0af714a77d3d003ae68fe2
danielclough <danielclough@gmail.com>
2020-03-03 19:51:44 -0800

    update copyright

--- src/gatsby-theme-musician/components/footer.js
+++ src/gatsby-theme-musician/components/footer.js
@@ -3,42 +3,16 @@
 import React from "react"
 import { Styled, jsx } from "theme-ui"
 
-import useSiteMetadata from "../use-site-metadata"
-import Social from "./social"
-
 export default ({ children }) => {
-  const { title, social } = useSiteMetadata()
-
-  let hasSocial = false
-  if (social.length) {
-    if (typeof social[0].name !== "undefined") {
-      if (social[0].name.length) {
-        hasSocial = true
-      }
-    }
-  }
 
   return (
     <footer role="contentinfo" sx={{ variant: "layout.footer" }}>
-      {hasSocial && (
-        <>
-          <strong
-            className="GtmFooter__social-heading"
-            sx={{ variant: "styles.caps" }}
-          >
-            Follow &amp; Subscribe
-          </strong>
-          <div className="GtmFooter__social-wrapper">
-            <Social />
-          </div>
-        </>
-      )}
       <hr sx={{ variant: "layout.footer.hr" }} />
       {children ? (
         <div sx={{ variant: "layout.footer.copyright" }}>{children}</div>
       ) : (
         <small sx={{ variant: "layout.footer.copyright" }}>
-          Copyright © {new Date().getFullYear()} {title}. 
+          Copyright © {new Date().getFullYear()} Wilmore & Company. 
           <br />
           Built by{" "}
           <Styled.a href="https://danielc.us/copyright" rel="external nofollow">
